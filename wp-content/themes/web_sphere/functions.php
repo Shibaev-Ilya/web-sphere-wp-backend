5097450712f1f0327ad3176201b29b01d554b69d
Ilia Shibaev <hausdr@gmail.com>
2026-01-08 12:31:45 +0300

    fix: enqueue scripts

--- wp-content/themes/web_sphere/functions.php
+++ wp-content/themes/web_sphere/functions.php
@@ -185,15 +185,15 @@ function web_sphere_scripts() {
 			get_template_directory_uri() . '/dist/separated-styles/text-page.css', array(), _S_VERSION, 'all' );
 	}
 
+	if (is_page_template('template-compare-tool.php')) {
+		wp_enqueue_style( 'web_sphere_style_compare-tool', get_template_directory_uri() . '/dist/separated-styles/compare-tool.css', array(), _S_VERSION, 'all' );
+	}
+
+	wp_enqueue_script( 'web_sphere_script_vendor', get_template_directory_uri() . '/dist/js/vendor.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'web_sphere_script_main', get_template_directory_uri() . '/dist/js/main.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
-	}
-
-	if (is_page_template('template-compare-tool.php')) {
-		wp_enqueue_style( 'web_sphere_style_compare-tool', get_template_directory_uri() . '/dist/separated-styles/compare-tool.css', array(), _S_VERSION, 'all' );
-		wp_enqueue_script( 'web_sphere_script_vendor', get_template_directory_uri() . '/dist/js/vendor.js', array(), _S_VERSION, true );
 	}
 }
 
